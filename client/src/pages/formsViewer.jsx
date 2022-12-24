e9f4a09728ede934875c381590a20bfea2330bc2
Sergey <67481268+aqude@users.noreply.github.com>
2022-12-24 23:28:35 +0300

    add delete button

--- client/src/pages/formsViewer.jsx
+++ client/src/pages/formsViewer.jsx
@@ -1,19 +1,29 @@
 import {db} from "../firebase.jsx";
-import {collection, getDocs} from "firebase/firestore";
+import {collection, getDocs, doc, deleteDoc} from "firebase/firestore";
 import {useEffect, useState} from "react";
 import "../assets/formsViewer.css";
+
 
 function formsViewer() {
     const [forms, setForms] = useState([]);
     useEffect(() => {
+        // вернуть с id документа
         getDocs(collection(db, "forms")).then((querySnapshot) => {
             const items = [];
             querySnapshot.forEach((doc) => {
-                items.push(doc.data());
+                items.push({...doc.data(), id: doc.id});
             });
             setForms(items);
-        });
+        } );
     } , []);
+    // удаление данных
+    const deleteData = (id) => {
+        alert("Данные успешно удалены " + id);
+        deleteDoc(doc(db, "forms", id));
+        setForms(forms.filter((item) => item.id !== id));
+    }
+
+
     return (
         <div>
             <h1 style={{textAlign: "center", marginBottom: 20}}>Закупочные товары</h1>
@@ -41,6 +51,9 @@ function formsViewer() {
                             <td className="">{form.Model}</td>
                             <td className="">{form.quantity}</td>
                             <td className="">{form.price}</td>
+                            <td className="td_btn">
+                                <button className="btn" onClick={() => deleteData(form.id)}>Удалить</button>
+                            </td>
                         </tr>
                     ))}
                 </tbody>
